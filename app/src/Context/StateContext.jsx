4fed49f4322a1bb25ad39dc51d0df021bb025650
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2023-11-18 18:42:38 +0100

    Injection d'une sécurité dans la fonction injectMessage()

--- app/src/Context/StateContext.jsx
+++ app/src/Context/StateContext.jsx
@@ -14,27 +14,42 @@ export const StateProvider = ({children}) => {
 
     const [messageMapIndex, setMessageMapIndex] = useState(0)
     const [messageMap, setMessageMap] = useState([
-        {id: 0, text:"Bonjour tout le monde", position: "beginning"},
-        {id: 1, text:"Je m'appelle Anthony", position: "beginning"},
+        {id: 0, text:"Bonjour visiteur.", position: "beginning"},
+        {id: 1, text:"Bienvenu sur le CV interactif d'Anthony.", position: "beginning"},
+        {id: 2, text:"Je m'appelle Roboto3000.", position: "beginning"},
     ])
 
     
 
     const injectMessage = () => {
-        setMessageDisplay("")
-        const messageIndex = messageMap.findIndex(message => message.id === messageMapIndex)
-        console.log(messageIndex)
-        let step = 0
-        const intervalID = setInterval(() => {
-            if(startTyping){
-                step++
-                setMessageDisplay(currentMessageDisplay => (currentMessageDisplay + messageMap[messageIndex].text[step - 1]))
-                if(step >= messageMap[messageIndex].text.length){
-                    clearInterval(intervalID)
-                    setMessageMapIndex(current => current + 1)
+        // On récupère l'index du message dans MAP qui correspond au step de message en cours
+        const indexOfThisMessageInMap = messageMap.findIndex(message => message.id === messageMapIndex)
+        // Sécurité pour eviter de cliquer vers un message qui n'existe pas
+        if(indexOfThisMessageInMap >= 0)
+        {
+            // On reset le message
+           setMessageDisplay("")
+           // On initialialise le step à 0
+            let step = 0
+            // On déclenche un interval basé sur le readingSpeed
+            const intervalID = setInterval(() => {
+                // Si startTyping est sur true ( après le firstDelay Déclencher dans Main.jsx)
+                if(startTyping){
+                    // On incrémente step
+                    step++
+                    // On change le message
+                    setMessageDisplay(currentMessageDisplay => (currentMessageDisplay + messageMap[indexOfThisMessageInMap].text[step - 1]))
+                    // Quand on arrive au bout du message
+                    if(step >= messageMap[indexOfThisMessageInMap].text.length){
+                        // On clear l'interval
+                        clearInterval(intervalID)
+                        // On change le step du message à afficher
+                        setMessageMapIndex(current => current + 1)
+                    }
                 }
-            }
-        }, readingSpeed);
+            }, readingSpeed); 
+        }
+        
     }
 
 
