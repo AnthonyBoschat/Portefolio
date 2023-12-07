99b7c4040a3b605af47e0a7a36b6304a8f18f16c
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2023-12-07 14:42:07 +0100

    Changement de la valeur de readingSpeed

--- app/src/Components/Options.jsx
+++ app/src/Components/Options.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import { StateContext } from "../Context/AppContext";
+import { GlobalParameterContext } from "../Context/GlobalParameterContext";
 
 function Options(){
 
     /////// STATE /////////
-    const {optionsState, dispatchOptions, ACTION_OPTIONS, dispatchStateGlobalParameter, ACTIONS_GLOBALPARAMETER} = useContext(StateContext)
-
+    const {optionsState, dispatchOptions, ACTION_OPTIONS} = useContext(StateContext)
+    const {ACTIONS_GLOBALPARAMETER, dispatchStateGlobalParameter, globalParameter} = useContext(GlobalParameterContext)
 
     /////// METHODE /////////
     const handleClickCloseOptions = (event) => {
@@ -23,10 +24,10 @@ function Options(){
         <div onClick={handleClickCloseOptions} className={`optionsOverlay ${optionsState.animationClass}`}>
             <div className="optionsBox">
                 <label htmlFor="optionReadingSpeed">Vitesse de lecture</label>
-                <select defaultValue="25" onChange={(e) => dispatchStateGlobalParameter({type: ACTIONS_GLOBALPARAMETER.UPDATE_READINGSPEED, payload:e.target.value})} id="optionReadingSpeed">
-                    <option value="15">Lent</option>
+                <select value={globalParameter.readingSpeed} onChange={(e) => dispatchStateGlobalParameter({type: ACTIONS_GLOBALPARAMETER.UPDATE_READINGSPEED, payload:e.target.value})} id="optionReadingSpeed">
+                    <option value="35">Lent</option>
                     <option value="25">Normal</option>
-                    <option value="35">Rapide</option>
+                    <option value="15">Rapide</option>
                 </select>
             </div>
         </div>
