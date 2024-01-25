437595b5646810f8d7cb41f68e62663541bfba36
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-01-25 23:37:02 +0100

    Mise en place des premières catégories

--- app/src/Features/Writter/CustomHook/useWhiteBar.jsx
+++ app/src/Features/Writter/CustomHook/useWhiteBar.jsx
@@ -4,28 +4,28 @@ import { useSelector } from "react-redux";
 export default function useWhiteBar(){
 
     const cursor = useSelector(store => store.writter.cursor)
-    const [cursorVisible, setcursorVisible] = useState(false)
+    const [cursorVisible, setcursorVisible] = useState(true)
 
     // Fonction qui fait clignotter la bare blanche qui sert de curseur lors du chargement
     const cursorBlink = () => {
         const intervalBlinkID = setInterval(() => {
             setcursorVisible(current => !current)
-        }, 500)
+        }, 300)
 
         return intervalBlinkID
     }
 
     // Quand l'état du curseur change, on fait clignotter l'état local cursorVisible
-    useEffect(() => {
-        let intervalBlinkID
-        if(cursor){
-            intervalBlinkID = cursorBlink()
+    // useEffect(() => {
+    //     let intervalBlinkID
+    //     if(cursor){
+    //         intervalBlinkID = cursorBlink()
 
-            return(() => {
-                clearInterval(intervalBlinkID)
-            })
-        }
-    }, [cursor])
+    //         return(() => {
+    //             clearInterval(intervalBlinkID)
+    //         })
+    //     }
+    // }, [cursor])
 
 
 
