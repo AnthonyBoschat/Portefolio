c16814b89f9a350e6d28f491fd1aa013f822b3a5
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-01-29 22:44:30 +0100

    Amelioration de l'animation avec un ajout d'un nuage de point multicolor central et une sphere plus petite à l'interieur

--- app/src/Components/Main/subComponents/Pending/subComponents/Sphere/Sphere.jsx
+++ app/src/Components/Main/subComponents/Pending/subComponents/Sphere/Sphere.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 
 const Sphere = () => {
@@ -8,25 +8,86 @@ const Sphere = () => {
         // Scène, caméra et renderer
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(75, mountRef.current.clientWidth / mountRef.current.clientHeight, 0.1, 1000);
+        camera.position.z = 15;
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize(mountRef.current.clientWidth, mountRef.current.clientHeight);
         mountRef.current.appendChild(renderer.domElement);
 
-        // Géométrie de la sphère et matériau
-        const geometry = new THREE.SphereGeometry(6, 45, 45);
-        const material = new THREE.PointsMaterial({ color: "white", size: 0.001 });
-        const sphere = new THREE.Points(geometry, material);
-        scene.add(sphere);
 
-        // Positionnement de la caméra
-        camera.position.z = 15;
+        
+        // const sphere1Geometry = new THREE.SphereGeometry(6, 90, 45);
+        // const sphere1Material = new THREE.PointsMaterial({ color: "blue", size: 0.01 });
+        // const sphere1 = new THREE.Points(sphere1Geometry, sphere1Material);
+        // scene.add(sphere1);
+
+        // // Géométrie de la plus petite sphère
+        // const sphere2Geometry = new THREE.SphereGeometry(3, 30, 15); // Une petite sphère
+        // const sphere2Material = new THREE.PointsMaterial({ color: "red", size: 0.01 } ); // Couleur rouge pour visibilité
+        // const sphere2 = new THREE.Points(sphere2Geometry, sphere2Material);
+        // scene.add(sphere2)
+
+
+        
+
+
+        // Géométrie de la plus grande sphère
+        const sphere1Geometry = new THREE.SphereGeometry(6, 160, 80);
+        const sphere1Material = new THREE.PointsMaterial({color: "blue",size: 0.01});
+        const sphere1 = new THREE.Points(sphere1Geometry, sphere1Material);
+        scene.add(sphere1);
+
+        // Géométrie de la plus petite sphère
+        const sphere2Geometry = new THREE.SphereGeometry(3, 80, 40); // Une petite sphère
+        const sphere2Material = new THREE.PointsMaterial({ color: "red", size: 0.01 } ); // Couleur rouge pour visibilité
+        const sphere2 = new THREE.Points(sphere2Geometry, sphere2Material);
+        scene.add(sphere2)
+
+
+
+
+
+        // Géométrie du carré central 
+        const centerPointGeometry = new THREE.SphereGeometry();
+        const vertices = [];
+        const colors = [];
+
+        // Ajouter des vertices et des couleurs
+        for (let i = 0; i < 500; i++) {
+            vertices.push(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
+            colors.push(Math.random(), Math.random(), Math.random());
+        }
+
+        // Ajouter des attributs de vertex et de couleur à la géométrie
+        centerPointGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+        centerPointGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+
+        const centerPointMaterial = new THREE.PointsMaterial({
+            size: 0.01,
+            // sizeAttenuation:true,
+            vertexColors:true,
+            });
+
+        const centerPoint = new THREE.Points(centerPointGeometry, centerPointMaterial);
+        scene.add(centerPoint);
+
 
         // Fonction d'animation
         const animate = () => {
             requestAnimationFrame(animate);
-            sphere.rotation.x += 0.005;
-            sphere.rotation.y += 0.008;
-            sphere.rotation.z += 0.005;
+
+            sphere1.rotation.x += 0.005;
+            sphere1.rotation.y += 0.010;
+            sphere1.rotation.z += 0.005;
+
+            sphere2.rotation.x -= 0.005;
+            sphere2.rotation.y -= 0.010;
+            sphere2.rotation.z -= 0.005;
+
+            centerPoint.rotation.x += 0.010;
+            centerPoint.rotation.y -= 0.020;
+            centerPoint.rotation.z += 0.010;
+            
+
             renderer.render(scene, camera);
         };
 
