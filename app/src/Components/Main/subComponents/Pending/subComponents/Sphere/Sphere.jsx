d69355b1f98ef2d4ed032239337a51030de3be0f
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-01-30 20:23:03 +0100

    Ajout d'une animation de zoom et dezoom

--- app/src/Components/Main/subComponents/Pending/subComponents/Sphere/Sphere.jsx
+++ app/src/Components/Main/subComponents/Pending/subComponents/Sphere/Sphere.jsx
@@ -4,6 +4,7 @@ import * as THREE from 'three';
 const Sphere = () => {
     const [startAnimation, setStartAnimation] = useState(false)
     const mountRef = useRef();
+    const cameraRef = useRef({})
 
     useEffect(() => {
         // Scène, caméra et renderer
@@ -13,19 +14,7 @@ const Sphere = () => {
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize(mountRef.current.clientWidth, mountRef.current.clientHeight);
         mountRef.current.appendChild(renderer.domElement);
-
-
-        
-        // const sphere1Geometry = new THREE.SphereGeometry(6, 90, 45);
-        // const sphere1Material = new THREE.PointsMaterial({ color: "blue", size: 0.01 });
-        // const sphere1 = new THREE.Points(sphere1Geometry, sphere1Material);
-        // scene.add(sphere1);
-
-        // // Géométrie de la plus petite sphère
-        // const sphere2Geometry = new THREE.SphereGeometry(3, 30, 15); // Une petite sphère
-        // const sphere2Material = new THREE.PointsMaterial({ color: "red", size: 0.01 } ); // Couleur rouge pour visibilité
-        // const sphere2 = new THREE.Points(sphere2Geometry, sphere2Material);
-        // scene.add(sphere2)
+        cameraRef.current = {camera}
 
 
         // Géométrie de la plus grande sphère
@@ -34,6 +23,7 @@ const Sphere = () => {
         const sphere1 = new THREE.Points(sphere1Geometry, sphere1Material);
         scene.add(sphere1);
 
+        
         // Géométrie de la plus petite sphère
         const sphere2Geometry = new THREE.SphereGeometry(3, 80, 40); // Une petite sphère
         const sphere2Material = new THREE.PointsMaterial({ color: "red", size: 0.01} ); // Couleur rouge pour visibilité
@@ -41,21 +31,19 @@ const Sphere = () => {
         scene.add(sphere2)
 
 
+
         // Géométrie du carré central 
         const centerPointGeometry = new THREE.SphereGeometry();
         const vertices = [];
         const colors = [];
-
         // Ajouter des vertices et des couleurs
         for (let i = 0; i < 500; i++) {
             vertices.push(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
             colors.push(Math.random(), Math.random(), Math.random());
         }
-
         // Ajouter des attributs de vertex et de couleur à la géométrie
         centerPointGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
         centerPointGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
-
         const centerPointMaterial = new THREE.PointsMaterial({
             size: 0.01,
             vertexColors:true,
@@ -84,19 +72,7 @@ const Sphere = () => {
 
             
 
-            const calculCameraDistance = (cameraPosition) => {
-                return cameraPosition / 100
-            }
-
-            if(startAnimation){
-                if(camera.position.z > 2){
-                    camera.position.z -= calculCameraDistance(camera.position.z)
-                }
-            }else if(!startAnimation){
-                if(camera.position < 15){
-                    camera.position.z += calculCameraDistance(camera.position.z)
-                }
-            }
+            
 
             renderer.render(scene, camera);
         };
@@ -110,9 +86,51 @@ const Sphere = () => {
                 mountRef.current.removeChild(renderer.domElement);
             }
         };
-    }, [startAnimation]);
+    }, []);
+
+
+    useEffect(() => {
+        let intervalID
+
+        
+        const {camera} = cameraRef.current
+
+        const adjustCamera = () => {
+            const calculCameraDistance = (cameraPosition) => {
+                return cameraPosition / 100
+            }
+    
+            if(startAnimation){
+                if(camera.position.z > 2){
+                    camera.position.z -= calculCameraDistance(camera.position.z)
+                    return false
+                }else{return true}
+            }else if(!startAnimation){
+                if(camera.position.z < 15){
+                    camera.position.z += calculCameraDistance(camera.position.z)
+                    return false
+                }else{return true}
+            }
+        }
+
+        intervalID = setInterval(() => {
+            const endOfAnimation = adjustCamera()
+            if(endOfAnimation){
+                clearInterval(intervalID)
+            }
+        }, 20);
+        
+
+        return () => clearInterval(intervalID)
+
+    }, [startAnimation])
+
 
     return <div onClick={() => setStartAnimation(current => !current)} className='sphereAnimation' ref={mountRef} style={{ width: '100%', height: '100%'}} />;
 };
 
 export default Sphere;
+
+
+
+
