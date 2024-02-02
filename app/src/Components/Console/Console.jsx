5ea5d051dacda2acb730c69f123d7eb39362c965
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-02-02 20:27:21 +0100

    Creation d'un composant Hexagon

--- app/src/Components/Console/Console.jsx
+++ app/src/Components/Console/Console.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Console({consoleConfiguration}){
 
     return(
-        <div ref={consoleConfiguration.presentationConsoleRef ? consoleConfiguration.presentationConsoleRef : null} className="mainCategoryPresentationConsoleBox">
-            <div className="pendingSentenceBox">
+        <div ref={consoleConfiguration.presentationConsoleRef ? consoleConfiguration.presentationConsoleRef : null} className="ConsoleBox">
+            <div className="typingSentenceBox">
                 {consoleConfiguration.typingSentence}
                 {consoleConfiguration.cursor ? consoleConfiguration.cursor : null}
             </div>
--- app/src/Components/Main/Main.jsx
+++ app/src/Components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import useMain from "./useMain";
-import TypingSentence_Contac from "../TypingSentence/Contact";
+import TypingSentence_Contact from "../TypingSentence/Contact";
 import TypingSentence_Profil from "../TypingSentence/Profil";
 import TypingSentence_Projet from "../TypingSentence/Projet";
 
@@ -15,7 +15,7 @@ function Main(){
                     <div className="mainCategoryList">
 
 
-                        <div onClick={() => navigateTo("Profil")} className="hexagon profilCategoryBox">
+                        <div onClick={() => navigateTo("Profil")} className="hexagon">
                             <svg viewBox="0 0 600 516.8">
                                 <polygon points="150.7,516.8 1.5,258.4 150.7,0 449,0 598.2,258.4 449,516.8 "/>
                             </svg>
@@ -26,7 +26,7 @@ function Main(){
                             </div>
                         </div>
 
-                        <div onClick={() => navigateTo("Projet")} className="hexagon projetCategoryBox">
+                        <div onClick={() => navigateTo("Projet")} className="hexagon">
                             <svg viewBox="0 0 600 516.8">
                                 <polygon points="150.7,516.8 1.5,258.4 150.7,0 449,0 598.2,258.4 449,516.8 "/>
                             </svg>
@@ -37,13 +37,13 @@ function Main(){
                             </div>
                         </div>
 
-                        <div onClick={() => navigateTo("/")} className="hexagon contactCategoryBox">
+                        <div onClick={() => navigateTo("/")} className="hexagon">
                             <svg viewBox="0 0 600 516.8">
                                 <polygon points="150.7,516.8 1.5,258.4 150.7,0 449,0 598.2,258.4 449,516.8 "/>
                             </svg>
                             <div className="categorySentenceBox">
                                 <div className="categoryName">
-                                    <TypingSentence_Contac/>
+                                    <TypingSentence_Contact/>
                                 </div>
                             </div>
                         </div>
