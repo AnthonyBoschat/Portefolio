b1626623f75ef134fa74cb44a441cf0778b8a4f2
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-02-17 18:26:31 +0100

    Petite animation pour l'out du focus du CV

--- app/src/Components/Scenes/CV/ZoomIn.jsx
+++ app/src/Components/Scenes/CV/ZoomIn.jsx
@@ -1,21 +1,39 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import Loading_CV from "../Loading/CV";
 
 function CVzoomIn({setCVfocus}){
 
 
     const boxRef = useRef()
     const height = window.innerHeight / 14
+    const overlayRef = useRef()
+    const [pdfOnload, setPdfOnload] = useState(true)
 
     const handleClickUnfocusCV = (event) => {
         if(event.target === event.currentTarget){
-            setCVfocus(current => !current)
+            if(boxRef.current){
+                boxRef.current.classList.add("opacity-out300")
+                setTimeout(() => {
+                    overlayRef.current.classList.add("opacity-out300")
+                    setTimeout(() => {
+                        setCVfocus(current => !current)
+                    }, 300);
+                }, 150);
+            }
         }
     }
 
+    const waitLoadPDF = () => {
+        setTimeout(() => {
+            setPdfOnload(false)
+        }, 1000);
+    }
+
     return(
-        <div onClick={handleClickUnfocusCV} className="cvFocusOverlay">
-            <div ref={boxRef} className="cvFocusBox">
-                <iframe src={`/Ressource/CVpdf.pdf#zoom=${height}`} alt="" />
+        <div ref={overlayRef} onClick={handleClickUnfocusCV} className="cvFocusOverlay">
+            {pdfOnload && (<Loading_CV/>)}
+            <div style={{display: pdfOnload ? "none" : "flex"}} ref={boxRef} className="cvFocusBox">
+                <iframe onLoad={waitLoadPDF} src={`/Ressource/CVpdf.pdf#zoom=${height}`} alt="" />
             </div>
         </div>
     )
