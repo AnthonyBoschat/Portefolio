581d6dbc4aca5db717f5208445a90f49685f225f
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-02-18 02:17:53 +0100

    Finission de l'animation pour le contact

--- app/src/Components/Scenes/Contact/Contact.jsx
+++ app/src/Components/Scenes/Contact/Contact.jsx
@@ -1,18 +1,56 @@
 import React, { useEffect, useRef, useState } from "react";
 import RotationLetter from "../../Constructors/RotationLetter/RotationLetter";
+import useContact from "./useContact";
 
 function Contact(){
 
+    const {
+        displayRef,
+        inputNameRef,
+        inputEmailRef,
+        inputMessageRef,
+        inputSubmitRef,
+        polylinesValues
+    } = useContact()
+
     return(
+        <div ref={displayRef} className="contactDisplay">
+            <div className="formBox">
 
+                <form action="">
 
-        <div className="testDisplay">
-            <div className="testBox">
-                <div className="test">
-                    <RotationLetter rotationConfiguration={{sentence:"Anthony"}}/>
-                </div>
+                    <div className="formDiv">
+                        <label htmlFor="name">Nom :</label>
+                        <input ref={inputNameRef} className="inputName" id="name" type="text" />
+                    </div>
+
+                    <div className="formDiv">
+                        <label htmlFor="email">Email :</label>
+                        <input ref={inputEmailRef} className="inputEmail" id="email" type="email" />
+                    </div>
+
+                    <div className="formDiv">
+                        <label htmlFor="message">Message :</label>
+                        <textarea ref={inputMessageRef} className="inputContent" id="message" />
+                    </div>
+                    
+                    <div className="formDiv">
+                        <input ref={inputSubmitRef} className="inputSubmit" type="submit" value={"Envoyer"} />
+                    </div>
+
+                </form>
             </div>
-        </div> 
+
+
+            <svg>
+                {polylinesValues.map(polyline => (
+                    <React.Fragment>
+                        <polyline className="polylineDisapear" strokeDashoffset={`${polyline.offset}%`} points={polyline.points.A}/>
+                        <polyline strokeDashoffset={`${polyline.offset}%`} points={polyline.points.B}/>
+                    </React.Fragment>
+                ))}
+            </svg>
+        </div>
     )
 }
 
--- app/src/Components/Scenes/Contact/useContact.jsx
+++ app/src/Components/Scenes/Contact/useContact.jsx
@@ -1,40 +1,84 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function useContact(){
 
-    const alphabet = "azertyuiopqsdfghjklmwxcvbn"
-    const [sentence, setSentence] = useState("")
-    const [step, setStep] = useState(0)
-    const finalSentence = "Ceci est une phrase vraiment très longue"
-    
+    const displayRef = useRef()
+    const inputNameRef = useRef()
+    const inputEmailRef = useRef()
+    const inputMessageRef = useRef()
+    const inputSubmitRef = useRef()
+
+    const [polylinesValues, setPolylinesValues] = useState([
+        {points:{A:null,B:null}, ref:inputNameRef, offsetGoal:380, offset:500, minus:0.001},
+        {points:{A:null,B:null}, ref:inputEmailRef, offsetGoal:380, offset:500, minus:0.01},
+        {points:{A:null,B:null}, ref:inputMessageRef, offsetGoal:380, offset:500, minus:0.8},
+        {points:{A:null,B:null}, ref:inputSubmitRef, offsetGoal:380, offset:500, minus:0.1},
+    ])
+
+
+    const calculatePolylinesPoints = (polylinesValuesArray) => {
+        const parentBounding = displayRef.current.getBoundingClientRect()
+        for(let i = 0; i<polylinesValuesArray.length; i++){
+            const childrenBounding = polylinesValuesArray[i].ref.current.getBoundingClientRect()
+            const A = 0
+            const B = childrenBounding.top
+            const C = childrenBounding.left - parentBounding.left
+            const D = B
+            const E = C + childrenBounding.width
+            const F = B
+            const G = E
+            const H = childrenBounding.top + childrenBounding.height
+            const I = C
+            const J = H
+            const K = C
+            const L = D
+            polylinesValuesArray[i].points.A = `${A},${B} ${C},${D}`
+            polylinesValuesArray[i].points.B = `${C},${D} ${E},${F} ${G},${H} ${I},${J} ${K},${L}`
+        }
+        return(polylinesValuesArray)
+    }
+
 
     useEffect(() => {
+        if(displayRef && inputNameRef && inputEmailRef && inputMessageRef && inputSubmitRef){
+            const copyPolylinesValues = [...polylinesValues]
+            const newPolylinesValues = calculatePolylinesPoints(copyPolylinesValues)
+            setPolylinesValues(newPolylinesValues)
+        }
+    }, [])
+
+    useEffect(() => {
+        
+        let offset = 500
         const intervalID = setInterval(() => {
-            
-            if(sentence != finalSentence){
-                const sentenceToShow = []
-                for(let i = 0; i<finalSentence.length; i++){
-                    const randomIndexAlphabet = Math.floor(Math.random() * alphabet.length) 
-                    sentenceToShow.push(alphabet[randomIndexAlphabet])
+            const copyPolylinesValues = [...polylinesValues]
+            let offsetEnd = true
+            for(let i = 0; i<copyPolylinesValues.length; i++){
+                if(offset > copyPolylinesValues[i].offsetGoal){
+                    offset -= 0.15
+                    copyPolylinesValues[i].offset = offset
+                    offsetEnd = false
                 }
-                setSentence(current => {
-                    for(let i = 0; i<step; i++){
-                        sentenceToShow[i] = finalSentence[i]
-                    }
-                    return sentenceToShow.join("")
-                })
-                setStep(current => current+1)
-                
-            }else{
-                clearInterval(intervalID)
             }
-        }, 15);
+
+            if(offsetEnd === true){
+                clearInterval(intervalID)
+            }else{
+                setPolylinesValues(copyPolylinesValues)
+            }
+        }, 10);
 
         return () => clearInterval(intervalID)
-    }, [sentence])
-    
+    }, [])
 
     return{
-        sentence
+        displayRef,
+        inputNameRef,
+        inputEmailRef,
+        inputMessageRef,
+        inputSubmitRef,
+        polylinesValues
     }
+        
+    
 }
