4db72e7043b55c5a6bf916db0e14daacb01a0a62
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-02-18 15:11:07 +0100

    Recalcul de la position des svg en cas de resize de l'écran.

--- app/src/Components/Scenes/Contact/useContact.jsx
+++ app/src/Components/Scenes/Contact/useContact.jsx
@@ -7,27 +7,29 @@ export default function useContact(){
     const inputEmailRef = useRef()
     const inputMessageRef = useRef()
     const inputSubmitRef = useRef()
+    const [offsetAnimationEnd, setOffsetAnimationEnd] = useState(false)
 
     const [polylinesValues, setPolylinesValues] = useState([
-        {points:{A:null,B:null}, ref:inputNameRef, offsetGoal:380, offset:500, minus:0.001},
-        {points:{A:null,B:null}, ref:inputEmailRef, offsetGoal:380, offset:500, minus:0.01},
-        {points:{A:null,B:null}, ref:inputMessageRef, offsetGoal:380, offset:500, minus:0.8},
+        {points:{A:null,B:null}, ref:inputNameRef, offsetGoal:400, offset:500, minus:0.001},
+        {points:{A:null,B:null}, ref:inputEmailRef, offsetGoal:400, offset:500, minus:0.01},
+        {points:{A:null,B:null}, ref:inputMessageRef, offsetGoal:300, offset:500, minus:0.8},
         {points:{A:null,B:null}, ref:inputSubmitRef, offsetGoal:380, offset:500, minus:0.1},
     ])
 
 
+    // Calcul la position des svg
     const calculatePolylinesPoints = (polylinesValuesArray) => {
         const parentBounding = displayRef.current.getBoundingClientRect()
         for(let i = 0; i<polylinesValuesArray.length; i++){
             const childrenBounding = polylinesValuesArray[i].ref.current.getBoundingClientRect()
             const A = 0
-            const B = childrenBounding.top
+            const B = childrenBounding.top - parentBounding.top
             const C = childrenBounding.left - parentBounding.left
             const D = B
             const E = C + childrenBounding.width
             const F = B
             const G = E
-            const H = childrenBounding.top + childrenBounding.height
+            const H = childrenBounding.top + childrenBounding.height - parentBounding.top
             const I = C
             const J = H
             const K = C
@@ -38,7 +40,7 @@ export default function useContact(){
         return(polylinesValuesArray)
     }
 
-
+    // Premier calcul des positions correcte pour les svg
     useEffect(() => {
         if(displayRef && inputNameRef && inputEmailRef && inputMessageRef && inputSubmitRef){
             const copyPolylinesValues = [...polylinesValues]
@@ -47,8 +49,9 @@ export default function useContact(){
         }
     }, [])
 
+
+    // Déclenche l'animation des svg en ajustant leurs offset
     useEffect(() => {
-        
         let offset = 500
         const intervalID = setInterval(() => {
             const copyPolylinesValues = [...polylinesValues]
@@ -63,6 +66,7 @@ export default function useContact(){
 
             if(offsetEnd === true){
                 clearInterval(intervalID)
+                setOffsetAnimationEnd(true)
             }else{
                 setPolylinesValues(copyPolylinesValues)
             }
@@ -71,7 +75,23 @@ export default function useContact(){
         return () => clearInterval(intervalID)
     }, [])
 
+
+
+    //Replace correctement les svg en cas de resize de l'écran
+    useEffect(() => {
+        const handleResizeSVG = () => {
+            const copyPolylinesValues = [...polylinesValues]
+            const newPolylinesValues = calculatePolylinesPoints(copyPolylinesValues)
+            setPolylinesValues(newPolylinesValues)
+        }
+
+        window.addEventListener("resize", handleResizeSVG)
+
+        return () => window.removeEventListener("resize", handleResizeSVG)
+    }, [])
+
     return{
+        offsetAnimationEnd,
         displayRef,
         inputNameRef,
         inputEmailRef,
