d5ae0722c5d145ceb903fcd20cf58db0011e0bed
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-02-22 20:38:43 +0100

    Finalisation de l'animation d'indication de mail envoyer

--- app/src/Components/Scenes/Svg/Contact_Animation/Contact_AnimationConfirm.jsx
+++ app/src/Components/Scenes/Svg/Contact_Animation/Contact_AnimationConfirm.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function Contact_AnimationConfirm(configuration){
 
-    
+    const IconeRef = useRef()
+
+    const handleClick = () => {
+        if(IconeRef.current){
+            IconeRef.current.classList.remove("animationClass_toggleCheck")
+            requestAnimationFrame(() => {
+                IconeRef.current.classList.add("animationClass_toggleCheck")
+            })
+        }
+    }
 
     return(
         <>
             <circle className="animationClass_circle_svgFill_In" strokeDashoffset={`${configuration.circle.dashOffset}%`} cx={configuration.circle.cx} cy={configuration.circle.cy} r={configuration.circle.r}/>
-            <polyline strokeDashoffset={`${configuration.polyline.dashOffset}%`} points={configuration.polyline.points} />
+
+
+            {/* <polyline strokeDashoffset={`${configuration.polyline.dashOffset}%`} points={configuration.polyline.points} /> */}
+
+
+            <foreignObject width="100%" height="100%">
+                <div onClick={handleClick}  style={{height:configuration.circle.r * 2, width:configuration.circle.r * 2, left:configuration.circle.cx / 2 + 10, top:configuration.circle.cy / 2 + 10}} className="checkIconeBox">
+                    <i ref={IconeRef} class="fa-solid fa-check"></i>
+                </div>
+            </foreignObject>
+            
+            
         </>
     )
 }
