1cc55d3f5f34d5219597297433907424bc5b9fee
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-02-24 12:06:49 +0100

    Création d'un composant Button_Projet

--- app/src/Components/Scenes/Projet/Projet.jsx
+++ app/src/Components/Scenes/Projet/Projet.jsx
@@ -1,16 +1,21 @@
 import React, { useRef } from "react";
 import useProjet from "./useProjet";
+import Button_Projet from "../Button/Projet/Projet";
 
 function Projet(){
 
-    const {projetConfiguration, generateProjetButton} = useProjet()
-
-    
+    const {projetConfiguration, setProjetConfiguration} = useProjet()
 
     return(
         <div className="projetDisplay">
             <div className="projetBox">
-                {projetConfiguration.map((projet, index) => generateProjetButton(projet, index))}
+                {projetConfiguration.map((projet, index) => 
+                    <Button_Projet 
+                    setProjetConfiguration={setProjetConfiguration}
+                    projetConfiguration={projetConfiguration}
+                    projet={projet}
+                    key={index}/>
+                )}
             </div>
         </div>
     )
--- app/src/Components/Scenes/Projet/useProjet.jsx
+++ app/src/Components/Scenes/Projet/useProjet.jsx
@@ -16,7 +16,7 @@ import redux from "../../../Assets/redux.png";
 export default function useProjet(){
 
 
-    const presentationBoxRef = useRef()
+    
     const programmationLanguage = {
         html:{image:html, name:"Html"},
         css:{image:css, name:"Css"},
@@ -89,60 +89,8 @@ export default function useProjet(){
     ])
     
 
-
-
-
-
-    const handleClickProjet = (projetName) => {
-        selectedButton(projetName)
-    }
-
-    const selectedButton = (projetName) => {
-        setProjetConfiguration(current => {
-            return current.map(projet => {
-                const copyProjet = {...projet}
-                if(copyProjet.name === projetName){
-                    copyProjet.selected = copyProjet.selected === false ? true : false
-                }else{
-                    copyProjet.selected = false
-                }
-                return copyProjet
-            })
-        })
-    }
-
-    const animationStyleClassCenter = (aButtonSelected, projet) => {
-        const index = projetConfiguration.findIndex(project => project.name === projet.name)
-        const indexButtonSelected = projetConfiguration.findIndex(projet => projet.selected === true)
-        const buttonStyle = aButtonSelected ? {top: 0 - (indexButtonSelected * 4.03) + "rem"} : {top:0 + "rem"}
-        const buttonClassName = !aButtonSelected ? "buttonProjet" : projet.selected ? "buttonProjetSelected" : "buttonProjetUnselected"
-        const projetStyle = aButtonSelected ? {top: 0 - (indexButtonSelected * 3.95) + "rem"} : {top:0 - (index * 3.95) + "rem"}
-        const projetClassName = projet.selected ? "projetPresentationVisible" : null
-        return{buttonStyle, buttonClassName, projetStyle, projetClassName}
-    }
-
-
-
-    const generateProjetButton = (projet, index) => {
-        
-        const aButtonSelected = projetConfiguration.find(projet => projet.selected === true)
-        const {buttonStyle, buttonClassName, projetStyle, projetClassName} = animationStyleClassCenter(aButtonSelected, projet)
-
-        return(
-            <React.Fragment key={index}>
-                <button style={buttonStyle} className={buttonClassName} onClick={() => handleClickProjet(projet.name)}>
-                    {projet.name}
-                </button>
-                <div ref={projet.selected ? presentationBoxRef : null} style={projetStyle} className={`projetPresentation ${projetClassName}`}>
-                    { projet.selected ? <Projet_Presentation projet={projet}/> : null}
-                </div>
-            </React.Fragment>
-            
-        )
-    }
-
     return{
         projetConfiguration,
-        generateProjetButton,
+        setProjetConfiguration
     }
 }
