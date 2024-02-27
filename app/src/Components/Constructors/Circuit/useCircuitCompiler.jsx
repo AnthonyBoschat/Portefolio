2469d9c626a79057fc52fc8d15a3af303aef393f
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-02-27 02:01:09 +0100

    Correction stupide

--- app/src/Components/Constructors/Circuit/useCircuitCompiler.jsx
+++ app/src/Components/Constructors/Circuit/useCircuitCompiler.jsx
@@ -19,21 +19,52 @@ export default function useCircuitCompiler(svgRef, circuitCenterRef){
     const createDivergences = (i, a, insertion, spaceX, spaceY) => {
         let divergences = ""
         const insertionSplit = insertion.split(",")
-        const insertionX = insertionSplit[0]
-        const insertionY = insertionSplit[1]
+        const insertionX = parseInt(insertionSplit[0])
+        const insertionY = parseInt(insertionSplit[1])
         console.log("insetionX => ",insertionX)
         console.log("insetionY => ",insertionY)
         if(i === 0){
             if(a === 1){divergences += `${insertionX - (spaceX / 2)},${insertionY - (spaceY / 2)}`}
-            // if(a === 2){}
-            // if(a === 3){}
-            // if(a === 4){}
-            // if(a === 5){}
-            // if(a === 6){}
-            // if(a === 7){}
-            // if(a === 8){}
-            // if(a === 9){}
+            if(a === 2){divergences += `${insertionX - (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 3){divergences += `${insertionX - (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 4){divergences += `${insertionX + (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 5){divergences += `${insertionX + (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 6){divergences += `${insertionX + (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 7){divergences += `${insertionX + (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 8){divergences += `${insertionX + (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 9){divergences += `${insertionX + (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+        }else if(i === 1){
+            if(a === 1){divergences += `${insertionX - (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 2){divergences += `${insertionX - (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 3){divergences += `${insertionX - (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 4){divergences += `${insertionX - (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 5){divergences += `${insertionX - (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 6){divergences += `${insertionX - (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 7){divergences += `${insertionX - (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 8){divergences += `${insertionX - (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 9){divergences += `${insertionX - (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+        }else if(i === 2){
+            if(a === 1){divergences += `${insertionX + (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 2){divergences += `${insertionX + (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 3){divergences += `${insertionX + (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 4){divergences += `${insertionX - (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 5){divergences += `${insertionX - (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 6){divergences += `${insertionX - (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 7){divergences += `${insertionX - (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 8){divergences += `${insertionX - (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 9){divergences += `${insertionX - (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+        }else if(i === 3){
+            if(a === 1){divergences += `${insertionX + (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 2){divergences += `${insertionX + (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 3){divergences += `${insertionX + (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 4){divergences += `${insertionX + (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 5){divergences += `${insertionX + (spaceX / 2)},${insertionY + (spaceY / 2)}`}
+            if(a === 6){divergences += `${insertionX + (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 7){divergences += `${insertionX + (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 8){divergences += `${insertionX + (spaceX / 2)},${insertionY - (spaceY / 2)}`}
+            if(a === 9){divergences += `${insertionX + (spaceX / 2)},${insertionY - (spaceY / 2)}`}
         }
+        
         
         return divergences
         
@@ -51,9 +82,6 @@ export default function useCircuitCompiler(svgRef, circuitCenterRef){
             const circuitBounding = circuitCenterRef.current.getBoundingClientRect()
             setSvgBounding(svgBounding)
             setCircuitBounding(circuitBounding)
-            console.log("0 => ", svgBounding, circuitBounding)
-        }else{
-            console.log("error")
         }
     }, [startCalcul])
 
@@ -71,60 +99,62 @@ export default function useCircuitCompiler(svgRef, circuitCenterRef){
             const bottomRightY = svgBounding.height / 2 + circuitBounding.height / 2 + 3 // On trouve la position bas droite du carré
             const spaceX = (svgBounding.width / 2 - circuitBounding.width / 2) / 10
             const spaceY = (svgBounding.width / 2 - circuitBounding.width / 2) / 10
+            const pourcentOfScale = 8
 
             const squarePoints = [] // On initialise un tableau vide qu'on va remplir de la position de tout les points
             for(let i = 0; i<4; i++){
                 switch(i){
                     case 0:
                         for(let a = 1; a<10; a++){
-                            let baseY = 80
-                            let differenceY = Math.abs(a-3) * 7
-                            if(a!==3){baseY -= differenceY}
+                            let baseY = 100
+                            let differenceY = Math.abs(a-4) * pourcentOfScale
+                            if(a!==4){baseY -= differenceY}
 
-                            // const insertion = `${topLeftX + (a * squarePointStep)},${topLeftY - (spaceY + ((10 - a) * (spaceY / 25)))}`
                             const goal = `${topLeftX + (a * squarePointStep)},${topLeftY}`
                             const insertion = `${topLeftX + (a * squarePointStep)},${topLeftY - baseY}`
-                            // const divergences = createDivergences(i, a, insertion, spaceX, spaceY)
-                            // let newPoint = `${divergences} ${insertion} ${goal} `
-                            let newPoint = `${insertion} ${goal} `
+                            const divergences = createDivergences(i, a, insertion, spaceX, spaceY)
+                            let newPoint = `${divergences} ${insertion} ${goal} `
                             squarePoints.push(newPoint)
                         }
                         continue
                     case 1:
                         for(let a = 1; a<10; a++){
-                            let baseX = 80
-                            let differenceX = Math.abs(a-7) * 7
-                            if(a!==7){baseX -= differenceX}
+                            let baseX = 100
+                            let differenceX = Math.abs(a-6) * pourcentOfScale
+                            if(a!==6){baseX -= differenceX}
 
 
                             const goal = `${topLeftX},${topLeftY  + (a * squarePointStep)}`
                             const insertion = `${topLeftX - baseX},${topLeftY + (a * squarePointStep)}`
-                            let newPoint = `${insertion} ${goal} `
+                            const divergences = createDivergences(i, a, insertion, spaceX, spaceY)
+                            let newPoint = `${divergences} ${insertion} ${goal} `
                             squarePoints.push(newPoint)
                         }
                         continue
                     case 2:
                         for(let a = 1; a<10; a++){
-                            let baseY = 80
-                            let differenceY = Math.abs(a-3) * 7
-                            if(a!==3){baseY -= differenceY}
+                            let baseY = 100
+                            let differenceY = Math.abs(a-4) * pourcentOfScale
+                            if(a!==4){baseY -= differenceY}
 
                             const goal = `${bottomRightX - (a * squarePointStep)},${bottomRightY}`
                             const insertion = `${bottomRightX - (a * squarePointStep)},${bottomRightY + baseY}`
-                            let newPoint = `${insertion} ${goal} `
+                            const divergences = createDivergences(i, a, insertion, spaceX, spaceY)
+                            let newPoint = `${divergences} ${insertion} ${goal} `
                             squarePoints.push(newPoint)
                         }
                         continue
                     case 3:
                         for(let a = 1; a<10; a++){
-                            let baseX = 80
-                            let differenceX = Math.abs(a-7) * 7
-                            if(a!==7){baseX -= differenceX}
+                            let baseX = 100
+                            let differenceX = Math.abs(a-6) * pourcentOfScale
+                            if(a!==6){baseX -= differenceX}
 
 
                             const goal = `${bottomRightX},${bottomRightY - (a * squarePointStep)}`
                             const insertion = `${bottomRightX + baseX},${bottomRightY - (a * squarePointStep)}`
-                            let newPoint = `${insertion} ${goal} `
+                            const divergences = createDivergences(i, a, insertion, spaceX, spaceY)
+                            let newPoint = `${divergences} ${insertion} ${goal} `
                             squarePoints.push(newPoint)
                         }
                         continue
